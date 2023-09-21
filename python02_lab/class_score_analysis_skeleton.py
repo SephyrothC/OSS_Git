60ec7fbe899efbf8913a2524aa00aaf0db3eb057
SephyrothC <as.grifon@gmail.com>
2023-09-21 03:54:44 +0200

    Update class_score_analysis_skeleton.py

--- python02_lab/class_score_analysis_skeleton.py
+++ python02_lab/class_score_analysis_skeleton.py
@@ -1,28 +1,64 @@
 def read_data(filename):
     # TODO) Read `filename` as a list of integer numbers
+    f = open(filename, "r")
     data = []
+    next(f)
+    for line in f :
+        txt = line.rstrip()
+        data.append(txt.rsplit(","))
+    for i in range(len(data)) :
+        data[i][0] = int(data[i][0])
+        data[i][1] = int(data[i][1])
     return data
 
 def calc_weighted_average(data_2d, weight):
     # TODO) Calculate the weighted averages of each row of `data_2d`
+
     average = []
+    x1 = 0.0
+    for row in range(len(data_2d)) :
+        for i in range(len(data_2d[row])) :
+            x1 += (float)(data_2d[row][i]) * weight[i]
+        average.append(x1)
+        x1 = 0
+
+          
     return average
 
 def analyze_data(data_1d):
     # TODO) Derive summary of the given `data_1d`
     # Note) Please don't use NumPy and other libraries. Do it yourself.
-    mean = 0
-    var = 0
+    
+    mean = 0.0
+    for i in range(len(data_1d)) :
+        mean += int(data_1d[i])
+    mean /= len(data_1d)
+
+    var = 0.0
+    for i in range(len(data_1d)) :
+        note = float(data_1d[i]) 
+        var += (note-mean)*(note-mean)
+    var /= len(data_1d)
+
     median = 0
-    return mean, var, median, min(data_1d), max(data_1d)
+    data_1d.sort()
+    if (len(data_1d)%2 == 0) :
+        n = len(data_1d)/2
+        m = n+1
+        median = (data_1d[n] + data_1d[m])/2
+    else:
+        n = int((len(data_1d)+1)/2)
+        median = data_1d[n]/2
+
+    return mean, var, median, (min((data_1d))), (max((data_1d)))
 
 if __name__ == '__main__':
-    data = read_data('data/class_score_en.csv')
+    data = read_data('python02_lab\data\class_score_en.csv')
     if data and len(data[0]) == 2: # Check 'data' is valid
         average = calc_weighted_average(data, [40/125, 60/100])
 
         # Write the analysis report as a markdown file
-        with open('class_score_analysis.md', 'w') as report:
+        with open('python02_lab\data\class_score_analysis.md', 'w') as report:
             report.write('### Individual Score\n\n')
             report.write('| Midterm | Final | Total |\n')
             report.write('| ------- | ----- | ----- |\n')
