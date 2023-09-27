835c24c662ad5ea73c929c61d52ef4ba948d8b2a
SephyrothC <as.grifon@gmail.com>
2023-09-27 10:19:38 +0200

    Enemy sprite

--- python03_lab/Zelda1_Survival_game/Main.py
+++ python03_lab/Zelda1_Survival_game/Main.py
@@ -1,5 +1,6 @@
 import pygame, time, random
 import botton
+import Menu
 
 pygame.font.init() #initialize font module
 
@@ -15,8 +16,8 @@ Player_position = (WIDTH/2-Player_Width, HEIGHT/2-Player_Height)
 Player_Velocity = 6
 
 #def the enemy
-Enemy_Width = 20
-Enemy_Height = 30
+Enemy_Width = 30
+Enemy_Height = 50
 Enemy_Velocity = 1
 
 #load the background
@@ -34,6 +35,12 @@ Player_Up = pygame.transform.scale(pygame.image.load("python03_lab\Zelda1_Surviv
 Player_Left = pygame.transform.scale(pygame.image.load("python03_lab\Zelda1_Survival_game\Sprites\Link_Left.png"), (Player_Width, Player_Height))
 Player_Right = pygame.transform.scale(pygame.image.load("python03_lab\Zelda1_Survival_game\Sprites\Link_Right.png"), (Player_Width, Player_Height))
 
+#Load Enemy
+Enemy_Down = pygame.transform.scale(pygame.image.load("python03_lab\Zelda1_Survival_game\Sprites\Enemy_Down.png"), (Enemy_Width, Enemy_Height))
+Enemy_Up = pygame.transform.scale(pygame.image.load("python03_lab\Zelda1_Survival_game\Sprites\Enemy_Up.png"), (Enemy_Width, Enemy_Height))
+Enemy_Left = pygame.transform.scale(pygame.image.load("python03_lab\Zelda1_Survival_game\Sprites\Enemy_Left.png"), (Enemy_Width, Enemy_Height))
+Enemy_Right = pygame.transform.scale(pygame.image.load("python03_lab\Zelda1_Survival_game\Sprites\Enemy_Right.png"), (Enemy_Width, Enemy_Height))
+
 #create botton
 x = pygame.Surface.get_width(Play_img) #all bottons have the same size
 y = pygame.Surface.get_height(Play_img) #all bottons have the same size
@@ -42,26 +49,28 @@ Score_Botton = botton.Button(WIDTH/2 - x/4, HEIGHT/2 + y/3, Score_img, 1/2)
 Quit_Botton = botton.Button(WIDTH/2 - x/4, HEIGHT - y/2 , Quit_img, 1/2)
 
 
-
-
 #def Font 
 FONT = pygame.font.SysFont("comicsans", 30)
 
 #Managing the display
-def draw(player, elapsed_time, enemies, direction):
+def draw(player, elapsed_time, enemies, direction_player, enemies_direction):
     #Background
     WIN.blit(BackGround_Game, (0,0))
 
     #Player
-    if(direction == 1) : WIN.blit(Player_Up, player)
-    if(direction == 2) : WIN.blit(Player_Right, player)
-    if(direction == 3) : WIN.blit(Player_Down, player)
-    if(direction == 4) : WIN.blit(Player_Left, player)
-
+    if(direction_player == 1) : WIN.blit(Player_Up, player)
+    if(direction_player == 2) : WIN.blit(Player_Right, player)
+    if(direction_player == 3) : WIN.blit(Player_Down, player)
+    if(direction_player == 4) : WIN.blit(Player_Left, player)
 
     #Enemies
+    i = 0
     for enemy in enemies[:] :
-        pygame.draw.rect(WIN, "blue", enemy)
+        if(enemies_direction[i] == 1) : WIN.blit(Enemy_Up, enemy)
+        if(enemies_direction[i] == 2) : WIN.blit(Enemy_Right, enemy)
+        if(enemies_direction[i] == 3) : WIN.blit(Enemy_Down, enemy)
+        if(enemies_direction[i] == 4) : WIN.blit(Enemy_Left, enemy)
+        i += 1
 
     #Time
     time_text = FONT.render(f"Time : {round(elapsed_time)}s", 1 , "white")
@@ -95,8 +104,6 @@ def Menu():
 
         
         pygame.display.update()
-        
-
 
 
 def main():
@@ -116,7 +123,7 @@ def main():
 
     #create a player
     player = Player_Down.get_rect(topleft = [Player_position[0], Player_position[1]])
-    direction = 3
+    direction_player = 3
 
     #create a enemy
     enemy_add_incresement = 2000
@@ -124,6 +131,7 @@ def main():
     enemy_dificulty = 2
 
     enemies = []
+    enemies_direction = []
     
 
     while run : #run the game
@@ -135,7 +143,7 @@ def main():
 
             #restart player
             player = Player_Down.get_rect(topleft = [Player_position[0], Player_position[1]])
-            direction = 3
+            direction_player = 3 #Down direction
 
             #restart time
             start_time = time.time()
@@ -144,7 +152,8 @@ def main():
             enemy_add_incresement = 2000
             enemy_count = 0
             enemy_dificulty = 2
-            enemies = []
+            enemies.clear()
+            enemies_direction.clear()
 
         #Set a clock tick and count for add enemies
         enemy_count += clock.tick(60)
@@ -162,26 +171,30 @@ def main():
                 if(start_enemy_pose == 1) : #top
                     enemy_y = -Enemy_Height
                     enemy_x = random.randint(0, WIDTH - Enemy_Width)
-                    enemy = pygame.Rect(enemy_x, enemy_y, Enemy_Width, Enemy_Height) #create a new enemy
+                    enemy = Enemy_Up.get_rect(topleft = [enemy_x, enemy_y]) #create a new enemy
                     enemies.append(enemy) #add the enemy to the enemies list
+                    enemies_direction.append(1)
 
                 if(start_enemy_pose == 2) : #right
                     enemy_y = random.randint(0, HEIGHT - Enemy_Height)
                     enemy_x = WIDTH + Enemy_Width
-                    enemy = pygame.Rect(enemy_x, enemy_y, Enemy_Width, Enemy_Height) #create a new enemy
+                    enemy = Enemy_Right.get_rect(topleft = [enemy_x, enemy_y]) #create a new enemy
                     enemies.append(enemy) #add the enemy to the enemies list
+                    enemies_direction.append(2)
 
                 if(start_enemy_pose == 3) : #bottom
                     enemy_y = HEIGHT + Enemy_Height
                     enemy_x = random.randint(0, WIDTH - Enemy_Width)
-                    enemy = pygame.Rect(enemy_x, enemy_y, Enemy_Width, Enemy_Height) #create a new enemy
+                    enemy = Enemy_Down.get_rect(topleft = [enemy_x, enemy_y]) #create a new enemy
                     enemies.append(enemy) #add the enemy to the enemies list
+                    enemies_direction.append(3)
 
                 if(start_enemy_pose == 4) : #left
                     enemy_y = random.randint(0, HEIGHT - Enemy_Height)
                     enemy_x = -Enemy_Width
-                    enemy = pygame.Rect(enemy_x, enemy_y, Enemy_Width, Enemy_Height) #create a new enemy
+                    enemy = Enemy_Left.get_rect(topleft = [enemy_x, enemy_y]) #create a new enemy
                     enemies.append(enemy) #add the enemy to the enemies list
+                    enemies_direction.append(4)
 
             enemy_add_incresement = max(200, enemy_add_incresement - 10) #augment the speed of the enemy spawn
             enemy_dificulty = min(8, enemy_dificulty+random.randint(0, 1)) #add more dificulty
@@ -198,24 +211,25 @@ def main():
         
         #Get the player key press and set deplacement 
         key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT] and player.x - Player_Velocity >= 0:
+        if key[pygame.K_LEFT] and player.x - Player_Velocity >= 0 :
             player.left -= Player_Velocity
-            direction = 4
+            direction_player = 4 
         elif key[pygame.K_RIGHT] and player.x + Player_Velocity + Player_Width <= WIDTH:
             player.right += Player_Velocity
-            direction = 2
+            direction_player = 2
         elif key[pygame.K_DOWN] and player.y + Player_Velocity + Player_Height <= HEIGHT:
             player.bottom += Player_Velocity
-            direction = 3
+            direction_player = 3
         elif key[pygame.K_UP] and player.y - Player_Velocity >= 0:
             player.top -= Player_Velocity
-            direction = 1
+            direction_player = 1
 
         #Pause game
         if key[pygame.K_ESCAPE] :
             load_menu = True
 
         #Add the enemy deplacement 
+        i = 0
         for enemy in enemies[:] :
             check_x = ((player.x+(player.width/2)) - (enemy.x+((enemy.width)/2)) ) 
             check_y = ((player.y+(player.height/2)) - (enemy.y+((enemy.height)/2)) ) 
@@ -224,23 +238,28 @@ def main():
             if check == 0  and check_y == 0 : check = check_x
             if check == check_y :
                 enemy.y += (check/abs(check))*Enemy_Velocity
+                if (check/abs(check)) == 1 :  enemies_direction[i] = 3
+                if (check/abs(check)) == -1 : enemies_direction[i] = 1
             elif check == check_x :
                 enemy.x += (check/abs(check))*Enemy_Velocity
-
+                if (check/abs(check)) == 1 :  enemies_direction[i] = 2
+                if (check/abs(check)) == -1 : enemies_direction[i] = 4
 
             if enemy.colliderect(player):
                 enemies.remove(enemy)
                 hit = True
                 break
+            i += 1
         
         if hit:
             lost_text = FONT.render(f"You lost!   Time : {round(elapsed_time)}s" , 1 , "white")
             WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
             pygame.display.update()
             pygame.time.delay(4000)
+            hit = False
             load_menu = True
 
-        draw(player, elapsed_time, enemies, direction) #draw the background, player, time elapsed
+        draw(player, elapsed_time, enemies, direction_player, enemies_direction) #draw the background, player, time elapsed
 
     pygame.quit()
 
