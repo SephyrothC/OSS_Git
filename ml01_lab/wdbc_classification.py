2bfbe39dfed2193c0c964e71d8f66e3d1770c132
SephyrothC <as.grifon@gmail.com>
2023-11-02 02:02:24 +0100

    wdbc classification

---
+++ ml01_lab/wdbc_classification.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import (datasets, svm, metrics)
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+
+from matplotlib.lines import Line2D  # For the custom legend
+import seaborn as sns
+
+
+def load_wdbc_data(filename):
+    class WDBCData:
+        data = []  # Shape: (569, 30)
+        target = []  # Shape: (569, )
+        target_names = ['malignant', 'benign']
+        feature_names = ['mean radius', 'mean texture', 'mean perimeter', 'mean area', 'mean smoothness', 'mean compactness', 'mean concavity', 'mean concave points', 'mean symmetry', 'mean fractal dimension',
+                         'radius error', 'texture error', 'perimeter error', 'area error', 'smoothness error', 'compactness error', 'concavity error', 'concave points error', 'symmetry error', 'fractal dimension error',
+                         'worst radius', 'worst texture', 'worst perimeter', 'worst area', 'worst smoothness', 'worst compactness', 'worst concavity', 'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    wdbc = WDBCData()
+    with open(filename) as f:
+        for line in f.readlines():
+            items = line.split(',')
+            # TODO #1) Add the true label (0 for M / 1 for others)
+            if (items[1] == 'M'):
+                wdbc.target.append(0)
+            else:
+                wdbc.target.append(1)
+            # TODO #1) Add 30 attributes (as floating-point numbers)
+            wdbc.data.append([float(item) for item in items[2:]])
+        wdbc.data = np.array(wdbc.data)
+    return wdbc
+
+
+if __name__ == '__main__':
+    # Load a dataset
+    # TODO #1) Implement 'load_wdbc_data()'
+    wdbc = datasets.load_breast_cancer()
+    # wdbc = load_wdbc_data('ml01_lab/data/wdbc.data')
+
+    # Train a model
+    # TODO #2) Find a better classifier (SVC accuracy: 0.902)
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(wdbc.data, wdbc.target)
+
+    # Test the model
+    predict = model.predict(wdbc.data)
+    accuracy = metrics.balanced_accuracy_score(wdbc.target, predict)
+
+    # TODO #3) Visualize the confusion matrix
+    # Calculate the confusion matrix
+    cm = confusion_matrix(wdbc.target, predict)
+    conf_disp = metrics.ConfusionMatrixDisplay(
+        cm, display_labels=['false', 'true'])
+    conf_disp.plot()
+
+    # Visualize testing results
+    cmap = np.array([(1, 0, 0), (0, 1, 0)])
+    clabel = [Line2D([0], [0], marker='o', lw=0, label=wdbc.target_names[i],
+                     color=cmap[i]) for i in range(len(cmap))]
+    for (x, y) in [(0, 1)]:  # Not mandatory, but try [(i, i+1) for i in range(0, 30, 2)]
+        plt.figure()
+        plt.title(f'My Classifier (Accuracy: {accuracy:.3f})')
+        plt.scatter(wdbc.data[:, x], wdbc.data[:, y],
+                    c=cmap[wdbc.target], edgecolors=cmap[predict])
+        plt.xlabel(wdbc.feature_names[x])
+        plt.ylabel(wdbc.feature_names[y])
+        plt.legend(handles=clabel, framealpha=0.5)
+    plt.show()
